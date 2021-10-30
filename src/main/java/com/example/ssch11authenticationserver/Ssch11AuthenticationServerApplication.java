c57e71706d2e349469d147d1ab57bdb946fbfcfc
Matt <matt@stallings.us>
2021-10-30 16:24:39 -0500

    Initial commit

---
+++ src/main/java/com/example/ssch11authenticationserver/Ssch11AuthenticationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssch11authenticationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssch11AuthenticationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssch11AuthenticationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ssch11authenticationserver/Ssch11AuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssch11authenticationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssch11AuthenticationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
