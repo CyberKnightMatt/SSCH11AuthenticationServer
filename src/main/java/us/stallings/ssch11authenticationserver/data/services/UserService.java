dbafc648d71cc9158cafd15898c2865f4aad1bfa
Matt <matt@stallings.us>
2021-10-30 16:51:48 -0500

    Added auth controller and supporting service.

---
+++ src/main/java/us/stallings/ssch11authenticationserver/data/services/UserService.java
@@ -0,0 +1,72 @@
+package us.stallings.ssch11authenticationserver.data.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import us.stallings.ssch11authenticationserver.data.entities.Otp;
+import us.stallings.ssch11authenticationserver.data.entities.User;
+import us.stallings.ssch11authenticationserver.data.repos.OtpRepository;
+import us.stallings.ssch11authenticationserver.data.repos.UserRepository;
+import us.stallings.ssch11authenticationserver.utils.GenerateCodeUtil;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private OtpRepository otpRepository;
+
+    public void addUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public void auth(User user) {
+        Optional<User> o = userRepository.findUserByUsername(user.getUsername());
+        if(o.isPresent()) {
+            User u = o.get();
+            if (passwordEncoder.matches(user.getPassword(), u.getPassword())) {
+                renewOtp(u);
+            } else {
+                throw new BadCredentialsException("Bad credentials.");
+            }
+        } else {
+            throw new BadCredentialsException("Bad credentials.");
+        }
+    }
+
+    private void renewOtp(User u) {
+        String code = GenerateCodeUtil.generateCode();
+        Optional<Otp> userOtp = otpRepository.findOtpByUsername(u.getUsername());
+        if (userOtp.isPresent()) {
+            Otp otp = userOtp.get();
+            otp.setCode(code);
+        } else {
+            Otp otp = new Otp();
+            otp.setUsername(u.getUsername());
+            otp.setCode(code);
+            otpRepository.save(otp);
+        }
+    }
+
+    public boolean check(Otp otpToValidate) {
+        Optional<Otp> userOtp = otpRepository.findOtpByUsername(otpToValidate.getUsername());
+
+        if (userOtp.isPresent()) {
+            Otp otp = userOtp.get();
+            if (otpToValidate.getCode().equals(otp.getCode())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
